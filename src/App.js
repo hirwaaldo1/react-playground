deeea34d04a99ff8af254d6f14c479c4da7dc911
hirwaaldo1 <hirwaaldo1@gmail.com>
2023-02-27 18:29:40 +0200

    feat learning about new oparation ??

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import React from "react";
 function App() {
-  const [{ x, y }, setXY] = React.useState({ x: 0, y: 100 });
+  let obj = { name: "Hirwa Aldo", age: 23 };
   return (
     <div>
-      <div style={{ display: "flex", width: "100%", backgroundColor: "red" }}>
-        <button
-          onClick={() => {
-            setXY({ x: x + 1, y });
-          }}
-        >
-          click
-        </button>
-        x:{x}
-        y:{y}
-      </div>
+      <div>{obj.name ?? "empty"}</div>
     </div>
   );
 }
