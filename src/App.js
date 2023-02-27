efd2403f94c47d317e2788adabc6156b5c5e94a8
hirwaaldo1 <hirwaaldo1@gmail.com>
2023-02-27 12:36:42 +0200

    feat: run two state in same hook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+import React from "react";
 function App() {
+  const [{ x, y }, setXY] = React.useState({ x: 0, y: 100 });
   return (
     <div>
-      <div
-        onClickCapture={(e) => {
-          e.stopPropagation();
-          alert("div");
-        }}
-        style={{ display: "flex", width: "100%", backgroundColor: "red" }}
-      >
-        <button onClick={() => alert("button")}>click</button>
+      <div style={{ display: "flex", width: "100%", backgroundColor: "red" }}>
+        <button
+          onClick={() => {
+            setXY({ x: x + 1, y });
+          }}
+        >
+          click
+        </button>
+        x:{x}
+        y:{y}
       </div>
     </div>
   );
