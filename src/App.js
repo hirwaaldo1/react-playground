0887732feacb9cab0dbeaa123a197701d47335f2
hirwaaldo1 <hirwaaldo1@gmail.com>
2023-03-08 13:00:43 +0200

    feat: learning ref in deep

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from "react";
+import { useRef, useEffect } from "react";
 function App() {
+  const ref = useRef();
   let obj = { name: "Hirwa Aldo", age: 23 };
+  useEffect(() => {
+    console.log(ref);
+  }, [ref]);
   return (
     <div>
-      <div>{obj.name ?? "empty"}</div>
+      <div ref={ref}>{obj.name ?? "empty"}</div>
     </div>
   );
 }
